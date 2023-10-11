75218fc1092c2899ad452894e4e4da06d45aced8
Zacc <55512911+Zlrobbins9@users.noreply.github.com>
2023-10-10 21:28:32 -0500

    Created script that allows words to follow you

---
+++ Intro/Assets/BadMessage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadMessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = GameObject.Find("Main Camera").transform.position;
+        this.transform.position = new Vector3(pos.x, pos.y, pos.z+3);
+        transform.Translate(0, 0, 2);
+    }
+}
