c53d601eda758c8b13bbd27acbdb1f8e608e9e54
Zacc <55512911+Zlrobbins9@users.noreply.github.com>
2023-10-22 16:21:48 -0500

    Added The floor puzzle!
    - when words are placed in the pits, they lose their ability to move and be grabbed
    - when 3 words are placed into the pits, the console will print an outcome based on how many of them are studying.

---
+++ Intro/Assets/FloorPuzzleController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorPuzzleController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    string p1Str = "N/A";
+    string p2Str = "N/A";
+    string p3Str = "N/A";
+    public string gameState = "playing";
+
+    void Start()
+    {
+        //platform1
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (p1Str.Equals("N/A"))
+        {  
+            p1Str = GameObject.Find("platform1").GetComponent<WordFloor>().ReturnPhrase();
+        }
+        if (p2Str.Equals("N/A"))
+        {
+            p2Str = GameObject.Find("platform2").GetComponent<WordFloor>().ReturnPhrase(); //this script exists inside platform 2
+        }
+        if (p3Str.Equals("N/A"))
+        {
+            p3Str = GameObject.Find("platform3").GetComponent<WordFloor>().ReturnPhrase();
+        }
+
+        if (!p1Str.Equals("N/A") && !p2Str.Equals("N/A") && !p3Str.Equals("N/A")) //All three platforms have words on them
+        {
+            Debug.Log("all 3 platforms have values");
+            int workValue = 0;
+            if (p1Str.Contains("Studying")) // First word
+            {
+                workValue++;
+            }
+            if (p2Str.Contains("Studying")) // First word
+            {
+                workValue++;
+            }
+            if (p3Str.Contains("Studying")) // First word
+            {
+                workValue++;
+            }
+            Debug.Log(workValue);
+            if (workValue == 2)
+            {
+                Debug.Log("you did it!");
+            }else if (workValue == 1)
+            {
+                Debug.Log("You didnt do enough work");
+            }
+            else if (workValue == 0)
+            {
+                Debug.Log("You didnt do any work :(");
+            }
+            else if (workValue == 3)
+            {
+                Debug.Log("You pushed yourself too hard and burnt out before finishing.");
+            }
+            else
+            {
+                Debug.Log("ERROR: recieving impossible value as a result of the floor puzzle. Impossible value:" + workValue);
+            }
+        }
+        else
+        {
+            Debug.Log(p1Str + " " + p2Str+ " " + " " +p3Str);
+        }
+    }
+}
--- Intro/Assets/WordFloor.cs
+++ Intro/Assets/WordFloor.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class WordFloor : MonoBehaviour
 {
+    string firstWordToCollide = "N/A";
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,24 @@ public class WordFloor : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //TODO: move the 'paralyzed' word to the center of the plank
+        if (firstWordToCollide == "N/A")
+        {
+            Debug.Log("First collision detected! It was with: " + collision.gameObject.name);
+            firstWordToCollide = collision.gameObject.name;
+            //TODO: take away the words' grabbable script
+            GameObject.Find(firstWordToCollide).GetComponent<GrabbableObject>().enabled = false;
+            GameObject.Find(firstWordToCollide).GetComponent<Rigidbody>().isKinematic = true;
+        }
+        
+    }
+
+    public string ReturnPhrase()
+    {
+        return firstWordToCollide;
     }
 }
