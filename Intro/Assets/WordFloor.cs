e721348cc8853774caad051eecf96611b24e8919
Zacc <55512911+Zlrobbins9@users.noreply.github.com>
2023-10-22 23:14:12 -0500

    Finished A7
    - level can now be reset when lost
    - words now hover after colliding with the bottom

--- Intro/Assets/WordFloor.cs
+++ Intro/Assets/WordFloor.cs
@@ -14,7 +14,10 @@ public class WordFloor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (GameObject.Find("platform2").GetComponent<FloorPuzzleController>().HasLost())
+        {
+            firstWordToCollide = "N/A";
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -25,6 +28,7 @@ public class WordFloor : MonoBehaviour
             Debug.Log("First collision detected! It was with: " + collision.gameObject.name);
             firstWordToCollide = collision.gameObject.name;
             //TODO: take away the words' grabbable script
+            GameObject.Find(firstWordToCollide).GetComponent<Transform>().position += new Vector3(0f,2f,0f);
             GameObject.Find(firstWordToCollide).GetComponent<GrabbableObject>().enabled = false;
             GameObject.Find(firstWordToCollide).GetComponent<Rigidbody>().isKinematic = true;
         }
---
+++ Intro/Assets/WordReset.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordReset : MonoBehaviour
+{
+    Vector3 startPos, startRot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = gameObject.transform.position;
+        startRot = gameObject.transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("Im disabled!");
+        GetComponent<GrabbableObject>().enabled = true;
+        GetComponent<Rigidbody>().isKinematic = false;
+        gameObject.transform.position = startPos;
+        gameObject.transform.eulerAngles = startRot;
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("Im enabled!");
+        
+        
+    }
+}
