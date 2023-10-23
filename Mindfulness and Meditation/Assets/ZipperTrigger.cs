e2f41c34d3d539b69b945452f1f8014e7e591fff
vtorres24 <145520186+vtorres24@users.noreply.github.com>
2023-10-23 01:21:53 -0500

    Added ZipperTrigger to Bag and deleted unused Assets

---
+++ Mindfulness and Meditation/Assets/ZipperTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZipperTrigger : MonoBehaviour
+{
+   public AudioClip mySound;
+void OnTriggerEnter (ZipperOpen other)
+	{
+	if(/*other.gameObject.tag =="Wand"*/Input.GetKey("z"))
+		{
+				GetComponent<AudioSource> ().PlayOneShot (mySound);
+		}
+	}
+}
