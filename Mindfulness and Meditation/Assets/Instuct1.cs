3b2b102c2f6d7b57bd29f9df1ca1dd9600b4bd51
vtorres24 <145520186+vtorres24@users.noreply.github.com>
2023-11-06 12:03:38 -0600

    updated text color and added audio

---
+++ Mindfulness and Meditation/Assets/Instuct1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instuct1 : MonoBehaviour
+{
+   public AudioClip mySound;
+   private bool isOpen = false;
+
+    void Start()
+    {
+     
+    }
+void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject.tag =="Player" && !isOpen) //collision occured
+		{
+			GetComponent<AudioSource> ().PlayOneShot (mySound);
+			isOpen = true;
+		}
+	}   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
